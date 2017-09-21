1504e19e3be818d861b2ed76696503a2743afb3f
lishouguang <lishouguang@163.com>
2017-09-21 15:09:58 +0800

    重构lm

--- clabel/model/lm.py
+++ clabel/model/lm.py
@@ -5,6 +5,9 @@ import re
 import jpype
 import codecs
 
+from abc import ABC
+from abc import abstractmethod
+
 from clabel.config import RESOURCE_DIR
 from clabel.config import LM_MODEL_DIR
 from clabel.nlp.pinyin import tag_pinyin
@@ -12,7 +15,7 @@ from clabel.nlp.pinyin import tag_pinyin
 from clabel.helper.utils import iter_file
 
 
-class LM(object):
+class LM(ABC):
     """
     Language Model
     基于BerkeleyLM实现
@@ -23,32 +26,58 @@ class LM(object):
         self._lm = JLM()
         self._lm.loadModel(model_file)
 
-    def predict_prob(self, txt):
-
+    @staticmethod
+    def preprocess(txt):
+        """
+        对待计算概率的句子进行预处理，只提取出汉字、英文、数字
+        :param txt:
+        :return:
+        """
         SYMBOL_ENG = '<eng>'
         SYMBOL_NUM = '<num>'
         SYMBOL_ENG_NUM = '<engnum>'
 
-        txt = re.sub(r'[，。？！?,]', '', txt)
+        tokens = re.findall(r'[a-zA-Z0-9\u4e00-\u9fa5]', txt)
 
-        tokens = tag_pinyin(txt)
+        tps = tag_pinyin(''.join(tokens))
 
-        words = []
-        for tp in tokens:
-            word = tp[0]
+        hanzis = []
+        pnyins = []
+        for tp in tps:
+            hanzi = tp[0]
+            pnyin = tp[1]
 
-            if re.match(r'^[a-zA-Z]+$', word):
-                word = SYMBOL_ENG
+            if re.match(r'^[a-zA-Z]+$', hanzi):
+                hanzi = SYMBOL_ENG
 
-            if re.match(r'^[0-9]+$', word):
-                word = SYMBOL_NUM
+            if re.match(r'^[0-9]+$', hanzi):
+                hanzi = SYMBOL_NUM
 
-            if re.match(r'^[a-zA-Z0-9]+$', word):
-                word = SYMBOL_ENG_NUM
+            if re.match(r'^[a-zA-Z0-9]+$', hanzi):
+                hanzi = SYMBOL_ENG_NUM
 
-            words.append(word)
+            hanzis.append(hanzi)
+            pnyins.append(pnyin if pnyin else hanzi)
 
-        return self._lm.getProb(' '.join(words))
+        return hanzis, pnyins
+
+    @abstractmethod
+    def predict_prob(self, txt):
+        pass
+
+
+class HanziLM(LM):
+
+    def predict_prob(self, txt):
+        hanzis, _ = LM.preprocess(txt)
+        return self._lm.getProb(' '.join(hanzis))
+
+
+class PinyinLM(LM):
+
+    def predict_prob(self, txt):
+        _, pnyins = LM.preprocess(txt)
+        return self._lm.getProb(' '.join(pnyins))
 
 
 def build_lm_train_data(raw_data_file, hanzi_data_file, pnyin_data_file):
@@ -65,9 +94,13 @@ def build_lm_train_data(raw_data_file, hanzi_data_file, pnyin_data_file):
     with codecs.open(hanzi_data_file, mode='w', encoding='utf-8') as hf,\
             codecs.open(pnyin_data_file, mode='w', encoding='utf-8') as pf:
 
+        j = 0
         for i, line in enumerate(iter_file(raw_data_file)):
-            if i % 10000 == 0:
-                print(i)
+            # if i % 10000 == 0:
+            #     print(i)
+
+            if j > 24713125:
+                break
 
             for sent in re.split(r'[，。？！?,]', line):
 
@@ -93,15 +126,19 @@ def build_lm_train_data(raw_data_file, hanzi_data_file, pnyin_data_file):
                     pnyin = pnyin if pnyin else word
                     pnyins.append(pnyin)
 
-                if words:
-                    hf.write('{}\n'.format(' '.join(words)))
+                # if words:
+                #     hf.write('{}\n'.format(' '.join(words)))
 
                 if pnyins:
-                    pf.write('{}\n'.format(' '.join(words)))
+                    j += 1
+                    if j % 10000 == 0:
+                        print(j)
+
+                    pf.write('{}\n'.format(' '.join(pnyins)))
 
 
 def run_test():
-    lm = LM(os.path.join(LM_MODEL_DIR, 'zh.arpa'))
+    lm = LM(os.path.join(LM_MODEL_DIR, 'hanzi.arpa'))
 
     from clabel.helper.utils import iter_file
     from clabel.helper.utils import write_file
@@ -118,10 +155,3 @@ def run_test():
     sort_probs = sorted(probs, key=lambda tp: tp[1])
 
     write_file(os.path.join(RESOURCE_DIR, 'tmp', 'result.txt'), ['{} {}'.format(p, s) for s, p in sort_probs])
-
-
-if __name__ == '__main__':
-    # run_test()
-    build_lm_train_data(os.path.join(RESOURCE_DIR, 'mobile', 'std.txt'),
-                        os.path.join(RESOURCE_DIR, 'tmp', 'std.hanzi.txt'),
-                        os.path.join(RESOURCE_DIR, 'tmp', 'std.pinyin.txt'))

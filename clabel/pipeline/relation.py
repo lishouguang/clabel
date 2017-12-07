1f8e685b043a349c9458280157af6b1e4c2414b3
lishouguang <lishouguang@meizu.com>
2017-12-07 15:47:03 +0800

    清理无关代码

--- clabel/pipeline/relation.py
+++ clabel/pipeline/relation.py
@@ -1,39 +1,14 @@
 # coding: utf-8
 
-import logging
 import os
-import re
+import logging
 
 from fastavro import writer
 
-from clabel.config import NLP_POS_SEPARATOR
 from common.utils import iter_file
-from nlp.lexicon import irrelevantLexicon
 from nlp.parser import default_parser as parser
 
 logger = logging.getLogger(__name__)
-
-__brands = irrelevantLexicon.get_words('brand')
-__models = irrelevantLexicon.get_words('model')
-__personals = irrelevantLexicon.get_words('personal')
-
-
-def pos(source_file, dest_file):
-    """
-    词性标注，标注之后进行修正
-    :param source_file: 原始评论
-    :param dest_file: 标注后的评论
-    """
-    logger.info('to pos comment text...')
-
-    tmp = dest_file + '.tmp'
-    with open(tmp, 'wb') as f:
-        for line in iter_file(source_file):
-            sents = parser.pos(line)
-            for sent in sents:
-                f.write('%s\n' % ' '.join(['%s%s%s' % (token, NLP_POS_SEPARATOR, pos) for token, pos in sent]))
-
-    correct_pos(tmp, dest_file)
 
 
 def parse(pinglun_file, dest_avro_file):
@@ -48,64 +23,41 @@ def parse(pinglun_file, dest_avro_file):
     if not os.path.exists(pdir):
         os.mkdir(pdir)
 
-    def pingluns():
-
-        for i, pinglun in enumerate(iter_file(pinglun_file)):
-            if i % 10000 == 0:
-                logger.info('parse line %d' % i)
-
-            if pinglun:
-
-                pinglun_obj = {
-                    'txt': pinglun,
-                    'sents': []
-                }
-
-                for sentence in parser.parse2sents(pinglun):
-                    tokens = set()
-
-                    for relation in sentence.relations:
-                        if relation.token1.word != 'Root':
-                            tokens.add(relation.token1)
-                        tokens.add(relation.token2)
-
-                    tokens = sorted(tokens, key=lambda t: t.id)
-
-                    pinglun_obj['sents'].append({
-                        'sent': ''.join([t.word for t in tokens]),
-                        'tokens': [{'word': t.word, 'pos': t.pos} for t in tokens],
-                        'relations': [{'format': r.format, 'token1': r.token1.word, 'token2': r.token2.word} for r
-                                      in sentence.relations]
-                    })
-
-                yield pinglun_obj
-
     with open(dest_avro_file, 'wb') as df:
-        writer(df, _PARSE_DATA_SCHEMA, pingluns())
+        writer(df, _PARSE_DATA_SCHEMA, _iter_parse_file(pinglun_file))
 
 
-def correct_pos(source_file, dest_file):
-    """
-    修正词性
-    :param source_file:
-    :param dest_file:
-    """
-    logger.info('to correct pos...')
+def _iter_parse_file(pinglun_file):
 
-    correct_map = {'BR': __brands, 'MO': __models, 'PE': __personals}
+    for i, pinglun in enumerate(iter_file(pinglun_file)):
+        if i % 10000 == 0:
+            logger.info('parse line %d' % i)
 
-    with open(dest_file, 'wb') as f:
-        for line in iter_file(source_file):
-            new_tps = list()
-            for tp in re.findall(r'(\S+)%s(\S+)' % NLP_POS_SEPARATOR, line):
-                new_tp = tp
+        if pinglun:
 
-                for k, v in list(correct_map.items()):
-                    if tp[0] in v:
-                        new_tp = (tp[0], k)
+            pinglun_obj = {
+                'txt': pinglun,
+                'sents': []
+            }
 
-                new_tps.append(new_tp)
-            f.write('%s\n' % ' '.join(['%s%s%s' % (tp[0], NLP_POS_SEPARATOR, tp[1]) for tp in new_tps]))
+            for sentence in parser.parse2sents(pinglun):
+                tokens = set()
+
+                for relation in sentence.relations:
+                    if relation.token1.word != 'Root':
+                        tokens.add(relation.token1)
+                    tokens.add(relation.token2)
+
+                tokens = sorted(tokens, key=lambda t: t.id)
+
+                pinglun_obj['sents'].append({
+                    'sent': ''.join([t.word for t in tokens]),
+                    'tokens': [{'word': t.word, 'pos': t.pos} for t in tokens],
+                    'relations': [{'format': r.format, 'token1': r.token1.word, 'token2': r.token2.word} for r
+                                  in sentence.relations]
+                })
+
+            yield pinglun_obj
 
 
 _PARSE_DATA_SCHEMA = {
